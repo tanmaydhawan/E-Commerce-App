6e2e473f1599844c04095b07868f413fd8460af0
tanmaydhawan <tanmaydhawan11@gmail.com>
2024-08-06 13:41:52 +0530

    Added JPA dependency, SQL configuration and Category entity.

--- src/main/java/com/springboot/ecommerce/model/Category.java
+++ src/main/java/com/springboot/ecommerce/model/Category.java
@@ -1,8 +1,17 @@
 package com.springboot.ecommerce.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class Category {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
 	private int categoryId;
+	
 	private String categoryName;
 
 	public Category() {
