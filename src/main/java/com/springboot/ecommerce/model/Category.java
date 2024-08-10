34286688c1e482cf7b6ccb397ef853f03af19d0f
tanmaydhawan <tanmaydhawan11@gmail.com>
2024-08-10 13:32:58 +0530

    Added Lombok dependency and Annotations.

--- src/main/java/com/springboot/ecommerce/model/Category.java
+++ src/main/java/com/springboot/ecommerce/model/Category.java
@@ -4,8 +4,14 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Category {
 
 	@Id
@@ -13,32 +19,5 @@ public class Category {
 	private int categoryId;
 	
 	private String categoryName;
-
-	public Category() {
-		super();
-		// TODO Auto-generated constructor stub
-	}
-
-	public Category(int categoryId, String categoryName) {
-		super();
-		this.categoryId = categoryId;
-		this.categoryName = categoryName;
-	}
-
-	public int getCategoryId() {
-		return categoryId;
-	}
-
-	public void setCategoryId(int categoryId) {
-		this.categoryId = categoryId;
-	}
-
-	public String getCategoryName() {
-		return categoryName;
-	}
-
-	public void setCategoryName(String categoryName) {
-		this.categoryName = categoryName;
-	}
 
 }
